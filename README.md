046cedd2cc5c411a76b563c3be813e94582ab922
h-hirokawa <h.hirokawa@realglobe.jp>
2016-07-25 20:37:42 +0900

    Update README. Refactor parse_ansible.py

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # autocomplete-ansible package
 
-An [autocomplete+](https://github.com/atom/autocomplete-plus) provider for Ansible Playbook, directives, modules autocompletation and module arguments snippet.
+An [autocomplete+](https://github.com/atom/autocomplete-plus) provider for Ansible Playbook.
+
+## Features
+* Autocompletion of Playbook directives & module names.
+* Snippets for module arguments.
+  * **module name + `a`**: Complete **required** arguments.
+  * **module_name + `o`**: Complete **all** arguments include optional.
 
 ![demo](https://cloud.githubusercontent.com/assets/1086022/16838450/1c3d368a-4a04-11e6-9775-64de7bf19dc2.gif)
+
+## Requirements
+* ansible >= 2.0
+
+## ToDo
+* Autocompletion of module arguments when the cursor is in module block.
+* Autocompletion of variables/factors.
--- lib/parse_ansible.py
+++ lib/parse_ansible.py
@@ -11,54 +11,53 @@ from ansible.playbook.task import  Task
 from ansible.plugins import lookup_loader, module_loader
 from ansible.utils import module_docs
 
-doc_cli = DocCLI([])
-module_paths = module_loader._get_paths()
+def main():
+    doc_cli = DocCLI([])
+    module_paths = module_loader._get_paths()
 
-module_keys = ('module', 'short_description', 'options', 'deprecated')
+    module_keys = ('module', 'short_description', 'options', 'deprecated')
 
-for path in module_paths:
-    doc_cli.find_modules(path)
+    for path in module_paths:
+        doc_cli.find_modules(path)
 
-result = {'modules': [], 'directives': {}}
+    result = {'modules': [], 'directives': {}}
 
-# モジュール情報の収集
-for module in sorted(set(doc_cli.module_list)):
-    if module in module_docs.BLACKLIST_MODULES:
-        continue
-    filename = module_loader.find_plugin(module, mod_type='.py')
-    if filename is None:
-        continue
-    if filename.endswith(".ps1"):
-        continue
-    if os.path.isdir(filename):
-        continue
-    try:
-        doc, plainexamples, returndocs = module_docs.get_docstring(filename)
-        filtered_doc = {key: doc.get(key, None) for key in module_keys}
-        result['modules'].append(filtered_doc)
-    except:
-        pass
-
-# ディレクティブを収集するクラス一覧
-class_list = [ Play, Role, Block, Task ]
-
-# ディレクティブ情報の収集
-for aclass in class_list:
-    aobj = aclass()
-    name = type(aobj).__name__
-
-    for attr in aobj.__dict__['_attributes']:
-        if 'private' in attr and attr.private:
+    for module in sorted(set(doc_cli.module_list)):
+        if module in module_docs.BLACKLIST_MODULES:
             continue
-        direct_target = result['directives'].setdefault(attr, [])
-        direct_target.append(name)
-        if attr == 'action':
-            local_action = result['directives'].setdefault('local_action', [])
-            local_action.append(name)
+        filename = module_loader.find_plugin(module, mod_type='.py')
+        if filename is None:
+            continue
+        if filename.endswith(".ps1"):
+            continue
+        if os.path.isdir(filename):
+            continue
+        try:
+            doc, plainexamples, returndocs = module_docs.get_docstring(filename)
+            filtered_doc = {key: doc.get(key, None) for key in module_keys}
+            result['modules'].append(filtered_doc)
+        except:
+            pass
 
-# ループ用lookupプラグインの収集
-for lookup in lookup_loader.all():
-    name = os.path.splitext(os.path.basename(lookup._original_path))[0]
-    result['directives']['with_' + name] = ['Task']
+    for aclass in (Play, Role, Block, Task):
+        aobj = aclass()
+        name = type(aobj).__name__
 
-print(json.dumps(result))
+        for attr in aobj.__dict__['_attributes']:
+            if 'private' in attr and attr.private:
+                continue
+            direct_target = result['directives'].setdefault(attr, [])
+            direct_target.append(name)
+            if attr == 'action':
+                local_action = result['directives'].setdefault('local_action', [])
+                local_action.append(name)
+
+    for lookup in lookup_loader.all():
+        name = os.path.splitext(os.path.basename(lookup._original_path))[0]
+        result['directives']['with_' + name] = ['Task']
+
+    print(json.dumps(result))
+
+
+if __name__ == '__main__':
+    main()
